2c139d8b6a15c6eb321f936d42a635f99691500e
chengtianhua <1057691751@qq.com>
2020-10-27 17:51:51 +0800

    cfrom 布局默认继承总的

--- package/sc-element/src/c-form/CForm.tsx
+++ package/sc-element/src/c-form/CForm.tsx
@@ -1,12 +1,12 @@
-import React, { useEffect } from 'react'
-import { Row, Form, Divider, Col } from 'antd'
-import _ from 'lodash'
-import ViewItem from './ViewItem'
+import React, { useEffect } from 'react';
+import { Row, Form, Divider, Col } from 'antd';
+import _ from 'lodash';
+import ViewItem from './ViewItem';
 
-const FormItem = Form.Item
+const FormItem = Form.Item;
 // const Panel = Page.PagePanel;
 
-const CForm: React.FC<any> = (props) => {
+const CForm: React.FC<any> = props => {
   const {
     formConfig = [],
     layout = 'horizontal',
@@ -17,52 +17,52 @@ const CForm: React.FC<any> = (props) => {
     action,
     form,
     ...respPorps
-  } = props
-  const formProps = { layout, labelCol, wrapperCol, labelAlign, ...respPorps }
-  const [waForm] = Form.useForm()
+  } = props;
+  const formProps = { layout, labelCol, wrapperCol, labelAlign, ...respPorps };
+  const [waForm] = Form.useForm();
 
   useEffect(() => {
     if (form && typeof form === 'function') {
-      form(waForm)
+      form(waForm);
     }
     if (form && typeof form !== 'function') {
-      form.current = waForm
+      form.current = waForm;
     }
-  }, [])
+  }, []);
 
   useEffect(() => {
     if (initialValues) {
-      waForm.setFieldsValue(initialValues)
+      waForm.setFieldsValue(initialValues);
     }
-  }, [initialValues])
+  }, [initialValues]);
 
   const convertData = (
     name: string,
     dataName: string,
     _props: any,
-    data: any
+    data: any,
   ) => {
-    let itemValue = data ? data[name] : null
-    let isDict = false
+    let itemValue = data ? data[name] : null;
+    let isDict = false;
     if (dataName && data) {
       if (data[`${dataName}Id`] || data[`${dataName}Name`]) {
         if (_props['textField'] && _props['valueField']) {
           itemValue = {
             [_props['textField']]: data[`${dataName}Name`],
             [_props['valueField']]: data[`${dataName}Id`],
-          }
+          };
         } else {
           itemValue = {
             key: data[`${dataName}Id`],
             title: data[`${dataName}Name`],
-          }
+          };
         }
-        isDict = true
+        isDict = true;
       }
     }
 
-    return { itemValue, isDict }
-  }
+    return { itemValue, isDict };
+  };
 
   // 创建表单项 {getFieldDecorator(name, { ...formProps })(React.createElement(component, { ...props }))}
   const createFormItem = (item: any) => {
@@ -74,38 +74,38 @@ const CForm: React.FC<any> = (props) => {
       dataName,
       hidden,
       readonly,
-    } = item
-    let _dataName = dataName
+    } = item;
+    let _dataName = dataName;
     if (typeof dataName === 'function') {
-      _dataName = dataName(initialValues, item)
+      _dataName = dataName(initialValues, item);
     }
 
-    const value = convertData(name, _dataName, item.props, initialValues)
-    const _name = name
-    const { itemValue, isDict } = value
+    const value = convertData(name, _dataName, item.props, initialValues);
+    const _name = name;
+    const { itemValue, isDict } = value;
     if (isDict && !item.props['data']) {
-      item.props['data'] = [itemValue]
+      item.props['data'] = [itemValue];
     }
     /* 输入框 统一添加allowClear 属性  */
     if (component && component.name === 'Input') {
-      item.props.allowClear = true
+      item.props.allowClear = true;
     }
     if (action === 'view') {
-      item.props.disabled = true
+      item.props.disabled = true;
     }
-    const itemProps = { label, ...formItemProps }
+    const itemProps = { label, ...formItemProps };
     if (hidden) {
       itemProps.style = {
         display: 'none',
-      }
+      };
     }
-    item.props.key = `form-item-component-${name}`
+    item.props.key = `form-item-component-${name}`;
     // eslint-disable-next-line no-nested-ternary
     const viewName: string = _dataName
       ? initialValues && initialValues[`${_dataName}Name`]
         ? `${_dataName}Name`
         : _dataName
-      : name
+      : name;
     return (
       <>
         {action === 'view' || readonly ? (
@@ -127,51 +127,51 @@ const CForm: React.FC<any> = (props) => {
           </FormItem>
         )}
       </>
-    )
-  }
+    );
+  };
 
   // 创建表单
   const createForm = (_formConfig: any) => {
     return _formConfig.map((item: any) => {
-      const { groupTitle, group, items, gutter = 16, col = 4 } = item
-      const colSpan = 24 / col
-      const defColProp = { span: colSpan, push: 0, pull: 0 }
-      let cols: any[] = []
-      const rows = []
-      let rowIndex = 0
-      let colCount = 0
+      const { groupTitle, group, items, gutter = 16, col = 4 } = item;
+      const colSpan = 24 / col;
+      const defColProp = { span: colSpan, push: 0, pull: 0 };
+      let cols: any[] = [];
+      const rows = [];
+      let rowIndex = 0;
+      let colCount = 0;
       items.forEach((formItem: any, index: number) => {
         if (!formItem) {
           cols.push(
             <Col
               key={`form-group-row${item.group}-${rowIndex} -${index}`}
               {...defColProp}
-            />
-          )
+            />,
+          );
         } else {
-          const { colProps, ...itemProps } = formItem
+          const { colProps, ...itemProps } = formItem;
 
-          const _props = { ...defColProp, ...colProps }
+          const _props = { ...defColProp, ...colProps };
           if (formItem.hidden) {
             _props.style = {
               display: 'none',
-            }
+            };
           }
-          colCount = colCount + props.span + props.push + props.pull
+          colCount = colCount + props.span + props.push + props.pull;
           // 设置默认的 栅格比例
           if (!itemProps.formItemProps || _.isEmpty(itemProps.formItemProps)) {
             itemProps.formItemProps = {
-              labelCol: { span: 8 },
-              wrapperCol: { span: 12 },
-            }
+              labelCol: labelCol,
+              wrapperCol: wrapperCol,
+            };
           } else {
             // eslint-disable-next-line no-shadow
-            const { labelCol, wrapperCol } = itemProps.formItemProps
+            const { labelCol, wrapperCol } = itemProps.formItemProps;
             if (!labelCol) {
-              itemProps.formItemProps['labelCol'] = { span: 8 }
+              itemProps.formItemProps['labelCol'] = labelCol;
             }
             if (!wrapperCol) {
-              itemProps.formItemProps['wrapperCol'] = { span: 12 }
+              itemProps.formItemProps['wrapperCol'] = wrapperCol;
             }
           }
           if (itemProps.component) {
@@ -181,34 +181,34 @@ const CForm: React.FC<any> = (props) => {
                 {..._props}
               >
                 {createFormItem(itemProps)}
-              </Col>
-            )
+              </Col>,
+            );
           }
         }
         if (colCount >= 24) {
-          rowIndex += 1
+          rowIndex += 1;
           rows.push(
             <Row
               gutter={gutter || 0}
               key={`form-group-row${item.group}-${rowIndex}`}
             >
               {cols}
-            </Row>
-          )
-          cols = []
-          colCount = 0
+            </Row>,
+          );
+          cols = [];
+          colCount = 0;
         }
-      })
+      });
       if (cols.length > 0) {
-        rowIndex += 1
+        rowIndex += 1;
         rows.push(
           <Row
             gutter={gutter || 0}
             key={`form-group-row-${item.group}-${rowIndex}`}
           >
             {cols}
-          </Row>
-        )
+          </Row>,
+        );
       }
       return (
         <div key={`form-group-${group}`} className="sc-form-group">
@@ -217,15 +217,15 @@ const CForm: React.FC<any> = (props) => {
           ) : null}
           {rows}
         </div>
-      )
-    })
-  }
+      );
+    });
+  };
 
   return (
     <Form form={waForm} {...formProps}>
       {createForm(formConfig)}
     </Form>
-  )
-}
+  );
+};
 
-export default CForm
+export default CForm;
