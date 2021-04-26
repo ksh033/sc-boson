e3e4be7e36df6c20af483a1a71b36c7241e4ee23
chengtianhua <1057691751@qq.com>
2021-04-26 14:30:58 +0800

    添加自定义PageContainer

---
+++ packages/layout/src/components/PageContainer/index.tsx
@@ -0,0 +1,34 @@
+/* eslint-disable react/no-array-index-key */
+import React, { useMemo } from 'react';
+import { PageContainer as APageContainer } from '@ant-design/pro-layout';
+import type { PageContainerProps } from '@ant-design/pro-layout';
+import { Button } from 'antd';
+
+const PageContainer: React.FC<PageContainerProps> = (props) => {
+  const { children, footer } = props;
+
+  const efooter = useMemo(() => {
+    /** 表单顶部合并 以及通用方法引入 */
+    let mergedFormButtons: React.ReactNode[] = [];
+    if (Array.isArray(footer)) {
+      mergedFormButtons = footer.map((item: any, index: number) => {
+        const buttonProps = item;
+        const { buttonType, text, ...resprops } = buttonProps;
+        return (
+          <Button key={`formButton${index}`} {...resprops}>
+            {text}
+          </Button>
+        );
+      });
+    }
+    return mergedFormButtons;
+  }, [footer]);
+
+  return (
+    <APageContainer {...props} footer={efooter}>
+      {children}
+    </APageContainer>
+  );
+};
+
+export default PageContainer;
