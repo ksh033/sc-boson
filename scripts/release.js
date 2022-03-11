31a5e7f76db26fee7096f9de74643e55e3db47b9
skimq <2072616987@qq.com>
2022-03-11 21:37:37 +0800

    修改打包

--- scripts/release.js
+++ scripts/release.js
@@ -143,7 +143,7 @@ async function release() {
       console.log(
         `[${index + 1}/${pkgs.length}] Publish package ${name} ${isNext ? 'with next tag' : ''}`,
       );
-      const cliArgs = isNext ? ['publish', '--tag', 'beta','--dry-run'] : ['publish'];
+      const cliArgs = isNext ? ['publish', '--tag', 'beta'] : ['publish'];
      const { stdout } = execa.sync('npm', cliArgs, {
         cwd: pkgPath,
      });
