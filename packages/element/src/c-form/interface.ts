1bf317e2ffb05614fa985a0d4229ecfef1b2f2cb
ksh033 <94456671@qq.com>
2022-04-28 18:21:21 +0800

    修改类型

--- packages/element/src/c-form/interface.ts
+++ packages/element/src/c-form/interface.ts
@@ -51,7 +51,7 @@ export interface FormItemProp {
    * 标准宽度，适用于大部分字段长度。 - lg=440px 适用于较长字段录入，如长网址、标签组、文件路径等。 - xl=552px
    * 适用于长文本录入，如长链接、描述、备注等，通常搭配自适应多行输入框或定高文本域使用。
    */
-  width?: number | 'sm' | 'md' | 'xl' | 'xs' | 'lg';
+  width?: number | 'sm' | 'md' | 'xl' | 'xs' | 'lg' | String;
   /** 是否只读 */
   readonly?: boolean;
   /** 是否隐藏 */
