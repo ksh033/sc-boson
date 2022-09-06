1fd56a7a28e986963c2cd9f611f1b364dac29d8a
chengtianhua <1057691751@qq.com>
2022-09-06 11:28:50 +0800

    ScTree格式化数据覆盖处理

--- packages/element/src/sc-tree/ScTree.tsx
+++ packages/element/src/sc-tree/ScTree.tsx
@@ -48,7 +48,10 @@ const ScTree: React.FC<ScTreeProps> = (props) => {
         let rIsLeaf = item.isLeaf;
         const otherAttr = { disabled: disabled || false, metaInfo, dataRef: dataRef || restItem };
         const title = render
-          ? render(dataRef || restItem)
+          ? render({
+              ...dataRef,
+              ...(restItem || {}),
+            })
           : item[textField || 'title'] || item.title;
         if (Array.isArray(children) && children.length > 0) {
           rChildren = formatTreeData(children);
@@ -70,7 +73,7 @@ const ScTree: React.FC<ScTreeProps> = (props) => {
   };
 
   const [treeData, setTreeData] = useMergedState<any[]>([], {
-    defaultValue: data,
+    value: data,
     onChange: onDataChange,
     postState: formatTreeData,
   });
